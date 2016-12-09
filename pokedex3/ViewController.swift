67acf804b350ac9444bb946b6c42cdb4942d109e
Mr Drew Westcott <code@drewwestcott.co.uk>
2016-12-09 15:29:02 +0000

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Drew Westcott on 09/12/2016.
+//  Copyright © 2016 Drew Westcott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
